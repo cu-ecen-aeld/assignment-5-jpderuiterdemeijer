5dd9c2ba711076137bb6599dc030f6607c04ca6d
jpderuiterdemeijer <jan.pieter.de.ruiter@airios.eu>
2023-06-05 06:13:28 -0600

    Fix gcc refs in writer makefile

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '8fce821c1b44aeec559fd1e242afb49c21392d89'
+AESD_ASSIGNMENTS_VERSION = '415727cff7b9c7fe6daf18300aaa87f5b2cbb5c2'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jpderuiterdemeijer.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
